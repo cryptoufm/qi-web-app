1ca67d622bd9b960ac43241791c63585fb5869c6
Juanro Alvarado <juanroalvarado@Juanros-MacBook-Pro.local>
2018-09-21 18:09:29 -0600

    Qis

--- contracts/Qi.sol
+++ contracts/Qi.sol
@@ -1,5 +1,18 @@
 pragma solidity ^0.4.17;
 
 contract Qi {
-  
+
+  address public owner;
+  string title;
+  uint date;
+  string other;
+
+  constructor (string title, string bday) public {
+
+	    userName=name;
+	    birthday=bday;
+
+	}
+
+
 }
--- contracts/qiRegistry.sol
+++ contracts/qiRegistry.sol
@@ -10,25 +10,35 @@ contract qiRegistry {
   // Mapping between profile and Q'i's it owns
   mapping (address => address[]) public receivers;
 
-  function validation(address qiAddress) {
+  function validation(address qiAddress) private{
     // Function for accessing Qi contract and verifying owner
     Qi q = Qi(qiAddress);
 		require(msg.sender == q.getOwner());
     return true
   }
 
-  function setEmmiter(address qiAddress) {
+  function setEmmiter(address qiAddress) public{
     // Validate that the message sender equals the Q'i' owner
-    require(validation(qiAddress))
+    require(validation(qiAddress));
     // Maps the Qi address to the Qi creator
     emitters[msg.sender].push(qiAddress);
   }
 
-  function setReceiver(address qiAddress, address receiverAddress){
+  function setReceiver(address qiAddress, address receiverAddress) public{
     // Validate that the message sender equals the Q'i' owner
-    require(validation(qiAddress))
+    require(validation(qiAddress));
     // Maps Qi address to the receiver's Qi list
     receivers[receiverAddress].push(qiAddress);
+  }
+
+  // Returns array of emitter Q'i's
+  function getEmitterQis(address emitterAddress){
+    return emitters[emitterAddress]
+  }
+
+  // Returns array of receiver Q'i's
+  function getReceiverQis(address receiverAddress){
+    return receivers[receiverAddress]
   }
 
 }
