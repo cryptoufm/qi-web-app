a62bc4afa6ed1624a3d3dfeacbaba66dd6ccc0a9
jbagur <juliorafaelbagur@gmail.com>
2018-09-22 00:24:17 -0600

    Migration 2

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,8 +1,24 @@
-var ConvertLib = artifacts.require("./ConvertLib.sol");
-var MetaCoin = artifacts.require("./MetaCoin.sol");
+var Collections = artifacts.require("./Collection.sol");
+var Migrations = artifacts.require("./Migrations.sol");
+var Qi = artifacts.require("./Qi.sol");
+var Owner = artifacts.require("./Owner.sol");
+var qiRegister = artifacts.require("qiRegistry")
+
+//Colletions
+var title = "academic";
+var alias = "juanro";
+
+//QI
+var _title = "distincion academica";
+var _other = "{\"text\":\"json\"}";
+
+//Owner
+var name = "Juan Roberto Bagur";
 
 module.exports = function(deployer) {
-  deployer.deploy(ConvertLib);
-  deployer.link(ConvertLib, MetaCoin);
-  deployer.deploy(MetaCoin);
+  deployer.deploy(Collections,title,alias);
+  deployer.deploy(Migrations);
+  deployer.deploy(Qi,_title,_other);
+  deployer.deploy(Owner,name);
+  deployer.deploy(qiRegister);
 };
