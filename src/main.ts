0285a2f31b0fe90b6861e0579ee2db325c7dba01
Edgar Godinez <edgarenrique.godinez@gmail.com>
2018-10-28 09:01:32 -0600

    Push final

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,9 @@ import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
-//import { environment } from './environments/environment';
+/*import { environment } from './environments/environment';
+  bendiciones 28/10/2018
+*/
 
 // if (environment.production) {
 //   enableProdMode();
